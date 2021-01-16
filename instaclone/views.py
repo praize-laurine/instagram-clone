87de3ed647563a8ab6a5e4a55894b23a0dca5188
praize-laurine <preyzelaurine@gmail.com>
2021-01-16 22:48:00 +0300

    installed superuser

--- instaclone/views.py
+++ instaclone/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http  import HttpResponse,Http404,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 
