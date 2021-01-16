a01fafb193ddfe36515994aeb791b431f948a908
praize-laurine <preyzelaurine@gmail.com>
2021-01-17 01:51:29 +0300

    created a view function for registration form

--- instapage/views.py
+++ instapage/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request,POST)
+        if form.is.valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get(password1)
+            user = authenticate(username=username, password=password)
+            login(request,user)
+            return redirect('index')
+
+        else:    
+            form = RegistrationForm()
+        return render(request, 'registration/register.html', {'form': form})    
