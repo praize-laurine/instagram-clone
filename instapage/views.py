fb72e373d994fea53bda61fdb0a10709881d8944
praize-laurine <preyzelaurine@gmail.com>
2021-01-17 03:02:47 +0300

    added index function

--- instapage/views.py
+++ instapage/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, redirect
+from django.http  import HttpResponse,Http404,HttpResponseRedirect
 from .forms import RegistrationForm
+from django.contrib.auth import authenticate, login,
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 def register(request):
@@ -16,3 +20,30 @@ def register(request):
         else:    
             form = RegistrationForm()
         return render(request, 'registration/register.html', {'form': form})    
+
+@login_required(login_url='/accounts/login/')
+def index(request):
+    images = Post.objects.all()
+    # users = User.objects.exclude(id=request.user.id)
+    json_posts = []
+    for post in images:
+
+        # import pdb; pdb.set_trace()
+        picture = Profile.objects.filter(user=post.user.id).first()
+        if picture:
+            picture = picture.profile_pic.url
+        else:
+            picture =''
+        obj = dict(
+            image=post.image.url,
+            author=post.user.username,
+            avatar=pic,
+            name=post.title,
+            caption=post.caption
+           
+
+        )
+        json_posts.append(obj)
+    return render(request, 'index.html', {"images": json_posts})
+
+
